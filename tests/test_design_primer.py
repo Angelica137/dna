2af407754123083b9a0378be094e06ef7b702177
Angelica <Angelica137@users.noreply.github.com>
2020-10-02 17:41:11 +0100

    test generate primer 2

--- tests/test_design_primer.py
+++ tests/test_design_primer.py
@@ -1,11 +1,16 @@
 from unittest import TestCase
 
-from app.design_primer import design_primer
+from app.design_primer import design_primer_one
 
 
 class TestDesginPrimer(TestCase):
 
     def test_return_primer_one(self):
-        result = design_primer('GGCGAGGAGCTG', 4)
+        result = design_primer_one('GGCGAGGAGCTG', 4)
         expected = 'GGCG'
+        self.assertEqual(result, expected)
+
+    def test_return_primer_two(self):
+        result = design_primer_two('GGCGAGGAGCTG', 4)
+        expected = 'GCTG'
         self.assertEqual(result, expected)
