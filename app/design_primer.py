13e7de2bdba1ed52ac10664b021fbdfd001c6d46
Angelica <Angelica137@users.noreply.github.com>
2020-10-02 17:43:19 +0100

    return last digits from end string

--- app/design_primer.py
+++ app/design_primer.py
@@ -1,4 +1,4 @@
-def design_primer(dna_region: str, primer_length: int):
+def design_primer_one(dna_region: str, primer_length: int):
 		"""
 		Design two primers for the reigion of interest in a given 
 		dna sequence
@@ -6,3 +6,13 @@ def design_primer(dna_region: str, primer_length: int):
 
 		primer_one = dna_region[0: primer_length]
 		return primer_one
+
+
+def design_primer_two(dna_region: str, primer_length: int):
+		"""
+		Design two primers for the reigion of interest in a given 
+		dna sequence
+		"""
+
+		primer_two = dna_region[-primer_length:]
+		return primer_two
